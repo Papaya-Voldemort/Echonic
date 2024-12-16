715a459e23b9c5d2e10c665493973d35512204f7
Papaya-Voldemort <165221493+Papaya-Voldemort@users.noreply.github.com>
2024-12-16 07:24:19 -0700

    Update nameGenerator.js

--- nameGenerator.js
+++ nameGenerator.js
@@ -1,23 +1,7 @@
 function generateMaleName() {
     const maleSyllables = ['el', 'ar', 'in', 'or', 'an'];
     return generateRandomName(maleSyllables);
-}
-
-function generateFemaleName() {
-    const femaleSyllables = ['ith', 'ul', 'en', 'as', 'ith'];
-    return generateRandomName(femaleSyllables);
-}
-
-function generateCityName() {
-    const citySyllables = ['dor', 'thal', 'mir', 'lan', 'nor'];
-    return generateRandomName(citySyllables);
-}
-
-function generateIslandName() {
-    const islandSyllables = ['ara', 'nua', 'tala', 'sara', 'luna'];
-    return generateRandomName(islandSyllables);
-}
-
+    
 function generateRandomName(syllables) {
     let name = '';
     const nameLength = Math.floor(Math.random() * 3) + 2; // Name length between 2 and 4 syllables
